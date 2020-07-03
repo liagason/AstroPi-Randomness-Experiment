8c32892058df4e5132b3bffb608c5d5d01e33143
Sotiris Liagas <liagason@gmail.com>
2020-07-03 10:20:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,40 +7,6 @@ from time import sleep
 from logzero import logger, logfile
 from sense_hat import SenseHat
 
-# ----------------------------------------------------
-# Team:               Entro PI
-# School:             17th Primary School of Agrinio
-# Country:            Greece
-# Teacher/Instructor: Liagkas Sotirios
-# Students:           Dipla Iliana
-#                     Bazoni Melina
-#                     Papachristos Evangelos
-#                     Serleme Lamprini
-#                     Tegas Georgios
-#                     Chrysanthakopoulou Alexadra
-# ----------------------------------------------------
-# README: The following code collects data from the
-# magnetometer/gyroscope/accelerometer/temperature/
-# pressure/humidity sensors and saves them in two
-# files (data01.csv, data03.csv). This data is used
-# as "seed" in order to produce random integers from
-# 0 to 9, that are saved in data02.csv and data04.csv.
-#
-# All values, in their raw form, are used as "seed"
-# to produce random integers using Python's Mersenne
-# Twister algorithm as generator. After that, these
-# values get "hashed", using Python's SHA256 hash
-# function, in order to get more entropic and then
-# used, again as "seed", to produce another series of
-# random integers between 0 and 9.
-#
-# The objective of this experiment is to collect data
-# in order to analyze it back on Earth and determine
-# if using values of natural phenomena as seed is an
-# effective way to produce random numbers in the
-# controlled enviroment of the ISS.
-# ----------------------------------------------------
-
 # Set file paths to save recorded and/or produced data.
 dir_path = os.path.dirname(os.path.realpath(__file__))
 logfile(dir_path + "/entro_pi.log")
